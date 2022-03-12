b9b37733ec7f577114cc918d96a74e3a2d0901d6
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-12 17:21:39 +0100

    'Undo' last refactoring (version 1) before alternate refactoring (version 2)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
+const reader = new CsvFileReader('football.csv');
 reader.read();
 
 let manUnitedWins = 0;
