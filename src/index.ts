f94524857c485046f44fadcaf850c8c8202a2851
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-14 13:56:31 +0100

    Create another reporting class - HtmlReport

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,12 @@ import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { WinsAnalysis } from './analyzers/WinsAnalisis';
 import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);
