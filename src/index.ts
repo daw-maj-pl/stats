314b11f2b544d81776cb5a8a9a43a39ae2f4e93c
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-14 15:12:47 +0100

    Implement winsAnalysisWithHtmlReport static method

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnalisis';
 import { Summary } from './Summary';
-import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
 summary.buildAndPrintReport(matchReader.matches);
