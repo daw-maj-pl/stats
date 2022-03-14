2edcc4ce51135ebe24eb2da3e599a02af2cced83
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-14 15:44:33 +0100

    Implement fromCsv static method

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
 import { Summary } from './Summary';
 
-const csvFileReader = new CsvFileReader('football.csv');
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
+
 matchReader.load();
 
 const summary = Summary.winsAnalysisWithHtmlReport('Man United');
